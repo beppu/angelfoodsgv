9a266c0489d1d4f2ea0b117e01de2031be5fd49d
John Beppu <john.beppu@gmail.com>
2010-12-19 12:20:16 -0800

    first pass at using a header and footer based on the theme called "throughout"

--- footer.php
+++ footer.php
@@ -1,2 +1,8 @@
+    </div>
+  </div>
+<div id="footer">
+  <p>Copyright (c) 2008 Sitename.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
+</div>
+<!-- end #footer -->
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,16 +1,55 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+Design by Free CSS Templates
+http://www.freecsstemplates.org
+Released for free under a Creative Commons Attribution 2.5 License
+
+Name       : Throughout 
+Description: A two-column, fixed-width design for 1024x768 screen resolutions.
+Version    : 1.0
+Released   : 20100423
+
+-->
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <title><?php echo $title ?></title>
-<link rel="stylesheet" type="text/css" href="/css/main.css" />
-<script type="text/javascript" src="/js/jquery-1.4.4.min.js"></script>
-<!--
-<link rel="stylesheet" type="text/css" href="FIXME" />
-<script type="text/javascript" src="FIXME"></script>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<script type="text/javascript" src="jquery/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="jquery/jquery.slidertron-0.1.js"></script>
+<link href="/css/style.css" rel="stylesheet" type="text/css" media="screen" />
 <style type="text/css">
-/* <![CDATA[ */
-/* ]]> */
+@import "slidertron.css";
 </style>
--->
 </head>
 <body>
+<!-- end #header-wrapper -->
+<div id="logo">
+  <h1><a href="#">Throughout </a></h1>
+  <p><em> template design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a></em></p>
+</div>
+<div id="header">
+  <div id="menu">
+    <ul>
+      <li><a href="#" class="first">Homepage</a></li>
+      <li class="current_page_item"><a href="#">Blog</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+  <!-- end #menu -->
+  <div id="search">
+    <form method="get" action="">
+      <fieldset>
+        <input type="text" name="s" id="search-text" size="15" />
+      </fieldset>
+    </form>
+  </div>
+  <!-- end #search -->
+</div>
+<!-- end #header -->
+<hr />
+<div id="page">
+  <div id="page-bgtop">
