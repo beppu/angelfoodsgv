046b979b8a216649d9620d4eb03dd384ffeb51be
John Beppu <john.beppu@gmail.com>
2010-12-24 16:26:40 -0800

    note a possible php bug

--- lib/init.php
+++ lib/init.php
@@ -2,7 +2,7 @@
 require_once('../configuration.php');
 require_once('helpers.php');
 require_once('db.php');
-require_once('../lib/menu.php'); # bug in require_once?
+require_once('../lib/menu.php'); # XXX - is this a bug in require_once?
 
 date_default_timezone_set('America/Los_Angeles');
 db();
