a92f6c213af6f3068de8f96b6e0a55ee40fd2b47
John Beppu <john.beppu@gmail.com>
2010-12-26 11:09:54 -0800

    don't store day of week

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,6 @@ create table menu_item (
   id int not null primary key auto_increment,
   menu_id int not null,
   day int not null,
-  dow char(10) not null,
   t enum('food', 'dismissal', 'holiday'),
   title varchar(32),
   body text,
