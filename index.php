d0484000cc351f794a1d8b37ed3ea37417f90005
John Beppu <john.beppu@gmail.com>
2010-12-28 17:17:28 -0800

    fleshing out order form

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include('header.php');
 ?>
 
 <?php
-$i = 0;
 $children = array(
   0 => "Jim",
   1 => "Bob",
@@ -19,9 +18,19 @@ $food_days = $menu->items('food');
 <h1>Order Form</h1>
 <div id="order-section">
   <form id="order" method="post" action="order.php">
+
+    <div class="family-info">
+      <h4>Family Name</h4>
+      <input type="text" name="family_name" size="30" />
+
+      <h4>Phone Number</h4>
+      <input type="text" name="phone_number" size="30" />
+    </div>
+
     <table class="mediagroove" cellspacing="0" cellpadding="0" width="100%">
       <thead>
         <tr>
+          <th>X</th>
           <th>Child's Name</th>
           <th>Grade</th>
           <th>Food</th>
@@ -33,41 +42,52 @@ $food_days = $menu->items('food');
       foreach ($children as $child) { 
       ?>
         <tr>
+          <td class="closer">X</td>
           <td>
-            <?php echo "$i $child" ?>
-            name
+            <input type="text" name="name" value="<?php echo "$child" ?>" />
           </td>
 
           <td>
-            grade
+            <select name="grade">
+              <option value="0">K</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+            </select>
           </td>
 
           <td>
             <ul class="picker">
             <?php foreach ($food_days as $item) { ?>
-              <li><?= $item->day < 10 ? "&nbsp;" : "" ?><?= $item->day ?></li>
+              <li class="none"><?= $item->day < 10 ? "&nbsp;" : "" ?><?= $item->day ?></li>
             <?php } ?>
             </ul>
           </td>
 
-          <td class="numeric">
-            price
-          </td>
+          <td class="numeric">$0.00</td>
         </tr>
       <?php
-        $i++;
       }
       ?>
       </tbody>
       <tfoot>
         <tr>
-          <th colspan="3">Total</th>
+          <td colspan="4">Total</th>
           <td colspan="1" class="numeric">$0.00</td>
         </tr>
       </tfoot>
     </table>
     <div id="buttons">
-      <input type="submit" name="submit" value="Purchase Lunches" />
+      <div class="add">
+        <input type="submit" name="submit" value="Add Child" />
+      </div>
+      <div class="purchase">
+        <input type="submit" name="submit" value="Purchase Lunches" />
+      </div>
+      <div class="clear">&nbsp;</div>
     </div>
   </form>
 </div>
--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@
   $(function(){
     // aesthetic
     $('table.mediagroove tbody tr:odd').addClass('odd');
+
+    // order form
+    {
+      $('ul.picker li').click(function(ev){
+        // none -> regular -> double -> none
+        var li = $(this);
+        if (li.hasClass('none')) {
+          li.removeClass('none').addClass('regular');
+        } else if (li.hasClass('regular')) {
+          li.removeClass('regular').addClass('double');
+        } else if (li.hasClass('double')) {
+          li.removeClass('double').addClass('none');
+        }
+      });
+    }
   });
 
 })(jQuery);
