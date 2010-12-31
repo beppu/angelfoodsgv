88b04b12bcad1e7b6620fb2879a351cfbb034cba
John Beppu <john.beppu@gmail.com>
2010-12-31 04:23:25 -0800

    a very dynamic order form

--- index.php
+++ index.php
@@ -8,15 +8,53 @@ include('header.php');
 
 <?php
 $children = array(
-  0 => "Jim",
-  1 => "Bob",
-  2 => "Mary",
-  3 => "Jane"
+  0 => "",
+  1 => "",
+  2 => "",
+  3 => ""
 );
 $food_days = $menu->items('food');
 ?>
+<div id="factory" style="display: none;">
+  <form>
+    <table>
+      <tbody>
+        <tr>
+          <td class="closer">X</td>
+          <td>
+            <input type="text" name="name[]" value="" />
+          </td>
+          <td>
+            <select name="grade[]">
+              <option value="-">-</option>
+              <option value="0">K</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+            </select>
+          </td>
+          <td>
+            <ul class="picker">
+            <?php foreach ($food_days as $item) { ?>
+              <li class="none"><?= $item->day < 10 ? "&nbsp;" : "" ?><?= $item->day ?></li>
+            <?php } ?>
+            </ul>
+            <span class="all">all</span>
+            <input type="hidden" name="days[]" />
+          </td>
+          <td class="numeric total">$0.00</td>
+        </tr>
+      </tbody>
+    </table>
+  </form>
+</div>
+
 <h1>Order Form</h1>
 <div id="order-section">
+
   <form id="order" method="post" action="order.php">
 
     <div class="family-info">
@@ -30,11 +68,11 @@ $food_days = $menu->items('food');
     <table class="mediagroove" cellspacing="0" cellpadding="0" width="100%">
       <thead>
         <tr>
-          <th>X</th>
-          <th>Child's Name</th>
-          <th>Grade</th>
+          <th width="20px">X</th>
+          <th width="20%">Child's Name</th>
+          <th width="30px">Grade</th>
           <th>Food</th>
-          <th>Price</th>
+          <th width="60px">Price</th>
         </tr>
       </thead>
       <tbody>
@@ -44,11 +82,12 @@ $food_days = $menu->items('food');
         <tr>
           <td class="closer">X</td>
           <td>
-            <input type="text" name="name" value="<?php echo "$child" ?>" />
+            <input type="text" name="name[]" value="<?php echo "$child" ?>" />
           </td>
 
           <td>
-            <select name="grade">
+            <select name="grade[]">
+              <option value="-">-</option>
               <option value="0">K</option>
               <option>1</option>
               <option>2</option>
@@ -65,9 +104,11 @@ $food_days = $menu->items('food');
               <li class="none"><?= $item->day < 10 ? "&nbsp;" : "" ?><?= $item->day ?></li>
             <?php } ?>
             </ul>
+            <span class="all">all</span>
+            <input type="hidden" name="days[]" />
           </td>
 
-          <td class="numeric">$0.00</td>
+          <td class="numeric total">$0.00</td>
         </tr>
       <?php
       }
@@ -76,13 +117,13 @@ $food_days = $menu->items('food');
       <tfoot>
         <tr>
           <td colspan="4">Total</th>
-          <td colspan="1" class="numeric">$0.00</td>
+          <td colspan="1" class="numeric grand-total">$0.00</td>
         </tr>
       </tfoot>
     </table>
     <div id="buttons">
       <div class="add">
-        <input type="submit" name="submit" value="Add Child" />
+        <a href="#buttons">Add Another Child</a>
       </div>
       <div class="purchase">
         <input type="submit" name="submit" value="Purchase Lunches" />
--- js/main.js
+++ js/main.js
@@ -5,20 +5,68 @@
     // aesthetic
     $('table.mediagroove tbody tr:odd').addClass('odd');
 
-    // order form
-    {
-      $('ul.picker li').click(function(ev){
-        // none -> regular -> double -> none
-        var li = $(this);
-        if (li.hasClass('none')) {
-          li.removeClass('none').addClass('regular');
-        } else if (li.hasClass('regular')) {
-          li.removeClass('regular').addClass('double');
-        } else if (li.hasClass('double')) {
-          li.removeClass('double').addClass('none');
+    // redo odd/even row colors
+    var alternatingRows = function(){
+      $('table.mediagroove tbody tr').removeClass('odd');
+      $('table.mediagroove tbody tr:odd').addClass('odd');
+    };
+
+    // recalculate totals
+    var totals = function(){
+      $('#order table.mediagroove tbody tr').each(function(i,tr){
+        var tr = $(tr);
+        var r = tr.find('ul.picker li.regular').length;
+        var d = tr.find('ul.picker li.double').length;
+        var total = ((r * 5) + (d * 6)).toFixed(2);
+        tr.find('td.total').html('$'+total);
+      });
+      var grandTotal = 0;
+      $('#order table.mediagroove tbody td.total').each(function(i, td){
+        var subTotal = parseFloat($(td).text().replace(/\$/, ''));
+        grandTotal += subTotal;
+      });
+      $('#order td.grand-total').html('$'+grandTotal.toFixed(2));
+    };
+
+    // deleting child
+    $('#order td.closer').live('click', function(ev){
+      if (confirm("Are you sure you want to remove this row?")) {
+        $(this).parents('tr').remove();
+        alternatingRows();
+        totals();
+      }
+    });
+
+    // choosing meals
+    $('#order ul.picker li').live('click', function(ev){
+      // none -> regular -> double -> none
+      var li = $(this);
+      if (li.hasClass('none')) {
+        li.removeClass('none').addClass('regular');
+      } else if (li.hasClass('regular')) {
+        li.removeClass('regular').addClass('double');
+      } else if (li.hasClass('double')) {
+        li.removeClass('double').addClass('none');
+      }
+      totals();
+    });
+
+    // choosing all meals
+    $('#order span.all').live('click', function(ev){
+      $(this).parents('tr').find('ul.picker li').each(function(i, li){
+        if ($(li).hasClass('none')) {
+          $(li).click();
         }
       });
-    }
+    });
+
+    // adding another child
+    $('#order div.add').click(function(ev){
+      $('#factory tr').clone().appendTo('#order table.mediagroove tbody');
+      alternatingRows();
+      return false;
+    });
+
   });
 
 })(jQuery);
