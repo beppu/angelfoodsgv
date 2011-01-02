5af62f8e1b4783ab9cad30232cfde919d3c30dd0
John Beppu <john.beppu@gmail.com>
2011-01-01 21:37:42 -0800

    schema updates for menu table

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,8 @@ create table menu (
   id int not null primary key auto_increment,
   year int not null,
   month int not null,
+  regular_price decimal(8,2),
+  double_price decimal(8,2),
   created_on datetime,
   modified_on timestamp,
   unique key (year, month)
@@ -15,9 +17,11 @@ create table menu_item (
   title varchar(32),
   body text,
   created_on datetime,
-  modified_on timestamp
+  modified_on timestamp,
+  unique key (menu_id, day)
 );
 
+-- This table is special, and it should only ever have one row in it.
 create table config (
   id int,
   current_menu_id int
