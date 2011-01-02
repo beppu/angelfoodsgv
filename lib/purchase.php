cbfd5a0d6b684857d4f8a03a6112b9c9d0b72486
John Beppu <john.beppu@gmail.com>
2011-01-02 03:31:19 -0800

    s/order/purchase/

--- lib/purchase.php
+++ lib/purchase.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * What is an order?
+ * What is a purchase?
  */
-class Order {
+class Purchase {
   public $id;
   public $menu_id;
   public $session_id;
@@ -14,24 +14,24 @@ class Order {
   public $modified_on;
 
   /**
-   * Retrieve an order from the database by id.
+   * Retrieve a purchase from the database by id.
    *
-   * @param   integer $id   the order id
+   * @param   integer $id   the purchase id
    * @return  Menu          a Menu object
    */
   static function find($id) {
-    $rs = mysql_query(sprintf("SELECT * FROM order WHERE id = %d", q($id)));
+    $rs = mysql_query(sprintf("SELECT * FROM purchase WHERE id = %d", q($id)));
     $menu = mysql_fetch_object($rs, 'Menu');
     return $menu;
   }
 
   /**
-   * Insert an order into the database.
+   * Insert an purchase into the database.
    *
    * @return  boolean       Was the insert successful?
    */
   function create() {
-    $rs = mysql_query(sprintf("INSERT INTO order (menu_id, session_id, family_name, phone_number, status, created_on) VALUES (%d, '%s', '%s', '%s', '%s', '%s')",
+    $rs = mysql_query(sprintf("INSERT INTO purchase (menu_id, session_id, family_name, phone_number, status, created_on) VALUES (%d, '%s', '%s', '%s', '%s', '%s')",
       q($this->menu_id), q($this->session_id), q($this->family_name), q($this->phone_number), q($this->status), now()
     ));
     $this->id = mysql_insert_id();
@@ -40,11 +40,11 @@ class Order {
 }
 
 /**
- * What is an order item?
+ * What is a purchase item?
  */
-class OrderItem {
+class PurchaseItem {
   public $id;
-  public $order_id;
+  public $purchase_id;
   public $day;
   public $child_name;
   public $child_grade;
