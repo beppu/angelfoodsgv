1d95926ad78cb34867b80d4d901693970845801d
John Beppu <john.beppu@gmail.com>
2011-01-02 03:29:41 -0800

    add session_id to order

--- lib/order.php
+++ lib/order.php
@@ -6,6 +6,7 @@
 class Order {
   public $id;
   public $menu_id;
+  public $session_id;
   public $family_name;
   public $phone_number;
   public $status;
@@ -30,8 +31,8 @@ class Order {
    * @return  boolean       Was the insert successful?
    */
   function create() {
-    $rs = mysql_query(sprintf("INSERT INTO order (menu_id, family_name, phone_number, status, created_on) VALUES (%d, '%s', '%s', '%s', '%s')",
-      q($this->menu_id), q($this->family_name), q($this->phone_number), q($this->status), now()
+    $rs = mysql_query(sprintf("INSERT INTO order (menu_id, session_id, family_name, phone_number, status, created_on) VALUES (%d, '%s', '%s', '%s', '%s', '%s')",
+      q($this->menu_id), q($this->session_id), q($this->family_name), q($this->phone_number), q($this->status), now()
     ));
     $this->id = mysql_insert_id();
     return $rs;
