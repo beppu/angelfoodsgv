f1c0d49bc63ddd0ae56a9059680c295a7c614b6d
John Beppu <john.beppu@gmail.com>
2011-01-02 03:29:26 -0800

    new tables purchase and purchase_item

--- db/schema.sql
+++ db/schema.sql
@@ -26,3 +26,28 @@ create table config (
   id int,
   current_menu_id int
 );
+
+create table purchase (
+  id int not null primary key auto_increment,
+  menu_id int not null,
+  session_id varchar(48) not null,
+  family_name varchar(48) not null,
+  phone_number varchar(20) not null,
+  status enum('pending', 'cancelled', 'paid') not null default 'pending',
+  created_on datetime,
+  modified_on timestamp,
+  unique key (menu_id, session_id)
+);
+
+create table purchase_item (
+  id int not null primary key auto_increment,
+  purchase_id int not null,
+  day int not null,
+  child_name varchar(48) not null,
+  child_grade int not null,
+  item_name varchar(48) not null,
+  item_price decimal(8,2) not null,
+  created_on datetime,
+  modified_on timestamp,
+  unique key (purchase_id, day)
+);
