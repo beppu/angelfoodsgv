ad855996046b27046678cf896be049262f9f8552
John Beppu <john.beppu@gmail.com>
2011-01-08 03:45:42 -0800

    implemented paypal_post()

--- lib/purchase.php
+++ lib/purchase.php
@@ -55,6 +55,78 @@ class Purchase {
     $item->id = mysql_insert_id();
     return $rs;
   }
+
+  /**
+   * Send a POST request to PayPal
+   *
+   * @param   string  $api_method   Name of PayPal API method
+   * @param   array   $params       POST parameters
+   * @param   array   $headers      HTTP headers
+   * @return  string                PayPal API response
+   */
+  function paypal_post($api_method, $params="", $headers=null) {
+    global $config;
+    $ch = curl_init($config->paypal_api_url);
+    if ($ch) {
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_VERBOSE, 1);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+      // Turn off the server and peer verification (TrustManager Concept).
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+      $params_as_string = "";
+      if (is_array($params)) {
+        foreach ($params as $k => $v) {
+          $params_as_string .= "&" . urlencode($k) . "=" . urlencode($v);
+        }
+      } else {
+        $params_as_string = $params;
+      }
+
+      $post_body = sprintf(
+        'METHOD=%s&VERSION=%s&USER=%s&PWD=%s&SIGNATURE=%s%s',
+        $api_method,
+        $config->paypal_api_version,
+        $config->paypal_api_user,
+        $config->paypal_api_password,
+        $config->paypal_api_signature,
+        $params_as_string
+      );
+
+      // Set the request as a POST FIELD for curl.
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+      $response = curl_exec($ch);
+      if (!$response) {
+        error_log("$api_method failed: ".curl_error($ch).'('.curl_errno($ch).')');
+        return false;
+      } else {
+        return $response;
+      }
+    } else {
+      error_log("$api_method failed: ".curl_error($ch).'('.curl_errno($ch).')');
+      return false;
+    }
+  }
+
+  /**
+   * Send purchase information to PayPal
+   *
+   * @return  string                PayPal redirect url
+   */
+  function paypal_checkout($options=null) {
+    $response = $this->paypal_post('SetExpressCheckout', array());
+  }
+
+  /**
+   * Issue a refund on this purchase
+   *
+   * @return ?
+   */
+  function paypal_refund($options=null) {
+    // TODO
+  }
 }
 
 /**
