d27ba671e30390c5268edcf8a524862766d6dd9e
John Beppu <john.beppu@gmail.com>
2011-01-10 13:52:58 -0800

    changed copyright year

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     </div>
   </div>
 <div id="footer">
-  <p>Copyright &copy; 2010 AngelFoodsGV.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
+  <p>Copyright &copy; 2011 AngelFoodsGV.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
 </div>
 <!-- end #footer -->
 </body>
