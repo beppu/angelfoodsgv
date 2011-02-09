cd19a242d4e85b181045b408eef0e4c78207db95
John Beppu <john.beppu@gmail.com>
2011-02-09 00:23:01 -0800

    this backup should work

--- admin/backup.php
+++ admin/backup.php
@@ -8,27 +8,18 @@ $tables = array(
   "purchase",
   "purchase_item"
 );
-
-$timestamp = strftime('%Y%m%d%H%M%S');
-
+$timestamp       = strftime('%Y%m%d%H%M%S');
 $backup_filename = "angelfoodsgv-backup-$timestamp.txt";
-$backup_dir = $config->db_backup_dir . "/$timestamp";
-
-mkdir($backup_dir);
-chmod($backup_dir, 0777);
-foreach ($tables as $t) {
-  $rs = mysql_query(sprintf("SELECT * FROM %s INTO OUTFILE '%s'", $t, sprintf("%s/%s.outfile", $backup_dir, $t)));
-}
 
 header("Content-Type: text/plain");
 header("Content-Disposition: attachment; filename=$backup_filename");
 
 foreach ($tables as $t) {
-  $f = sprintf("%s/%s.outfile", $backup_dir, $t);
   echo "$t\n";
-  readfile($f);
-  unlink($f);
+  $rs = mysql_query(sprintf("SELECT * FROM %s", $t));
+  while ($columns = mysql_fetch_array($rs, MYSQL_NUM)) {
+    echo join("\t", array_values($columns)) . "\n";
+  }
   echo "\n";
 }
-rmdir($backup_dir);
 ?>
