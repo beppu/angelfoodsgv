43735ccb25a408b60c708dbebeb1bb2f75f05ec2
John Beppu <john.beppu@gmail.com>
2011-05-13 20:37:02 -0700

    content for about page

--- about_content.php
+++ about_content.php
@@ -1,8 +1,45 @@
 <div class="top-section">
   <div class="informational">
-    <h1>About Us</h1>
-    <p>
-      Under Construction
-    </p>
+
+    <h1>Angel Foods</h1>
+    <h3>Family information</h3>
+
+    <h2>Welcome Families!</h2>
+    <p>Good nutrition is only living a healthy lifestyle and the goal of
+    providing nutritious meals for kids requires a group effort between the
+    family, the school and caterer.</p>
+    <div>
+      <img src="/images/angelfoodsgv_01.png" />
+    </div>
+
+    <h2>Commitment to quality</h2>
+    <p>We believe in the commitment of a good quality fresh fruits and
+    vegetables, the preparation of the food with good quality of
+    ingredients.</p>
+    <div>
+      <img src="/images/angelfoodsgv_02.png" />
+    </div>
+
+    <h2>Simple is Best</h2>
+    <p>Children’s nutrition is the cornerstone of what we do. We know
+    children’s palates are not developed as teens or adults so we take great
+    strides to ensure the menus are simple yet tasty. We serve the foods you
+    grew up on and we do it in proper portions with healthy ingredients to
+    create a menu that is balanced and appealing for the kid.</p>
+    <div>
+      <img src="/images/angelfoodsgv_03.png" />
+    </div>
+
+    <h2>Allergies &amp; Special Needs</h2>
+    <p>We are allergy conscious provider. Although we cannot guarantee an allergen free environment, we do are best to ensure the safety of our products. For specific questions or concerns, contact our office prior to ordering.</p>
+
+    <h2>Ordering</h2>
+    <p>Families may place orders on line at <a href="http://www.angelfoodsgv.com/">www.angelfoodsGv.com</a> or by delivering
+    the order to the school for pick-up. Order will not be processed without the
+    payment and all orders or charges must be received a day before the service for
+    processing. Or you can call on the morning before 11: am on the same day in
+    case of an emergency lunch. Please call marilu <b>(562) 556-6563</b> Thank
+    you!</p>
+
   </div>
 </div>
