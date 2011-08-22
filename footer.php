d88356cc0311104152dd57861e6a2dc46613b4da
John Beppu <beppu@bavl.org>
2011-08-22 03:24:54 +0000

    s/angelfoodsgv.com/angelfoodsgv.net/

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   </div>
 <div id="footer">
   <p>If you have any questions, please call Marilu.  <b>(562) 556-6563</b></p>
-  <p>Copyright &copy; 2011 AngelFoodsGV.com. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
+  <p>Copyright &copy; 2011 AngelFoodsGV.net. All rights reserved. Design by <a href="http://www.freecsstemplates.org/">Free CSS Templates</a>.</p>
 </div>
 <!-- end #footer -->
 </body>
