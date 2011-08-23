945df56ed0b4a45409d7dff175e7a16955121406
John Beppu <john.beppu@gmail.com>
2011-08-22 21:05:20 -0700

    see what google is posting to us

---
+++ notifications.php
@@ -0,0 +1,7 @@
+<?php
+
+error_log("_____________________________________________________________________________\n");
+error_log("GET:  " . var_export($_GET, true));
+error_log("POST: " . var_export($_POST, true));
+
+?>
