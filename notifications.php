b19b66990e186c846aedcf6e8f77ddc922a55995
John Beppu <john.beppu@gmail.com>
2011-08-23 15:17:20 -0700

    outline for what has to be done for notification handling

--- notifications.php
+++ notifications.php
@@ -4,4 +4,14 @@ error_log("_____________________________________________________________________
 error_log("GET:  " . var_export($_GET, true));
 error_log("POST: " . var_export($_POST, true));
 
+# get serial number
+
+# ask for notification via serial number
+
+# parse notification
+
+# figure out what to do with notification
+
+# send google an xml response letting them know the notification has been handled
+
 ?>
