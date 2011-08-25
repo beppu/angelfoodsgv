cd811590ea1f0310d6eb3d018fc28f4d6ccc9781
John BEPPU <john.beppu@gmail.com>
2011-08-25 09:25:45 -0700

    just messing around

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,21 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "AngelfoodsGV"
+set :repository,  "git@github.com:beppu/angelfoodsgv.git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "bavl.org"                          # Your HTTP server, Apache/etc
+role :app, "bavl.org"                          # This may be the same as your `Web` server
+role :db,  "bavl.org", :primary => true # This is where Rails migrations will run
+role :db,  "bavl.org"
+
+namespace :my do
+  desc "My first task"
+  task :hello, :roles => :app do
+    run "echo hello"
+    puts "world"
+  end
+end
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
