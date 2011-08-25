ec96ed40ca1d762412a09033635a7147c66ef7f0
John Beppu <john.beppu@gmail.com>
2011-08-25 07:51:03 -0700

    db modifications

--- db/0001_up.sql
+++ db/0001_up.sql
@@ -1,1 +1,1 @@
-alter table purchase add column google_serial_number varchar(64) default NULL after receipt_id;
+alter table purchase add column google_order_number varchar(64) default NULL after receipt_id;
---
+++ db/0002_up.sql
@@ -0,0 +1,12 @@
+create table notification (
+  id int not null primary key auto_increment,
+  purchase_id int default NULL,
+  google_serial_number varchar(64),
+  google_order_number varchar(64),
+  type varchar(32) not null,
+  timestamp timestamp,
+  xml text not null,
+  is_handled tinyint(1) default 0,
+  created_on datetime,
+  modified_on timestamp
+);
