ee6d6c32291242b665a36847864e1d6f9ef6a6c1
John BEPPU <john.beppu@gmail.com>
2011-10-09 18:35:04 -0700

    used a css3 button style for [Add Another Child]
    - https://github.com/ubuwaits/css3-buttons/blob/gh-pages/css/buttons.css
    - also tweaked some spacing

--- index.php
+++ index.php
@@ -145,7 +145,8 @@ $food_days = $menu->items('food');
     </table>
     <div id="buttons">
       <div class="add">
-        <a href="#buttons">Add Another Child</a>
+        <!-- <a href="#buttons">Add Another Child</a> -->
+        <button class="blue-pill">Add Another Child</button>
       </div>
       <div class="purchase">
         <!-- <input type="image" name="submit" src="https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif" /> -->
